cc33565c9c5631f7b658ebe8f73694e850237a8f
Steven Griffin <steven@bufferbaby.cyberpointllc.com>
2019-07-24 10:17:26 -0400

    multiprocessing and threading work

--- mp_example.py
+++ mp_example.py
@@ -1,12 +1,12 @@
-import queue
-import threading
+from multiprocessing import Process, Queue
 import time
 
-q = queue.Queue()
-frame_length = 0.2
+def f(q):
+    q.put([42, None, 'hello'])
 
 
-def pull_from_queue():
+
+def pull_from_queue(q, frame_length):
     target_time = time.monotonic()
     while True:
         target_time += frame_length
@@ -14,11 +14,10 @@ def pull_from_queue():
         if item is None:
             break
         print(item)
-        q.task_done()
         end = time.monotonic()
         time.sleep(max(0, target_time - time.monotonic()))
 
-def add_to_queue():
+def add_to_queue(q, frame_length):
     while True:
         start = time.monotonic()
         for i in range(5):
@@ -26,8 +25,15 @@ def add_to_queue():
         end = time.monotonic()
         time.sleep(frame_length * 5 - (end - start))
 
-pull_thread = threading.Thread(target=pull_from_queue)
-pull_thread.start()
-add_thread = threading.Thread(target=add_to_queue)
-add_thread.start()
+
+if __name__ == '__main__':
+    q = Queue()
+    frame_length = 0.2
+    pull_proc = Process(target=pull_from_queue, args=(q, frame_length))
+    pull_proc.start()
+    add_proc = Process(target=add_to_queue, args=(q, frame_length))
+    add_proc.start()
+
+    pull_proc.join()
+    add_proc.join()
 
