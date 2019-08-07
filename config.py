9c8df1067b431108082bab4c40c7b1879c0ddfe6
DAV <davalente@dreamport.tech>
2019-08-07 10:31:58 -0400

    fixed ip and stuff

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-ip = "192.168.2.100"
+ip = "192.168.1.109"
 port = "80"
 username = "admin"
-password = "c00lestinternsever"
+password = "internsarethebest"
 
 EMBEDDING_PATH = "../face-recognition/nn4.small2.v1.t7"
 RECOGNIZER_PATH = "../face-recognition/output/recognizer.pickle"
